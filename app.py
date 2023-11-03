ce121d7f3d816d26149e4152b079494f50f027e8
matthieugirard <matthieugirard@outlook.com>
2023-11-03 09:20:36 +0100

    add debug

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+import os
 from flask import Flask
 from dotenv import load_dotenv
 from controllers.document import document_controller
 
 load_dotenv()
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
 app = Flask(__name__)
 app.register_blueprint(document_controller)
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    if(DEBUG):
+        app.run(debug=True)
+    else:
+        app.run()
