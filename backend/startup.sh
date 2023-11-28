c8c40e58979cfec7e3fd08b7b0ada0bc841187ad
Your Name <you@example.com>
2023-11-28 02:25:55 +0100

    fix startup file

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,11 +1,11 @@
 wget -O - https://www.openssl.org/source/openssl-1.1.1u.tar.gz | tar zxf -
+apt-get update
+apt-get install build-essential libssl-dev ca-certificates libasound2 wget
 cd openssl-1.1.1u
 ./config --prefix=/usr/local
 make -j $(nproc)
-sudo make install_sw install_ssldirs
-sudo ldconfig -v
+make install_sw install_ssldirs
+ldconfig -v
 export SSL_CERT_DIR=/etc/ssl/certs
 rm -rf openssl-1.1.1u
-sudo apt-get update
-sudo apt-get install build-essential libssl-dev ca-certificates libasound2 wget
 gunicorn --bind 0.0.0.0 --threads 8 --timeout 600 --access-logfile '-' --error-logfile '-' app:app
