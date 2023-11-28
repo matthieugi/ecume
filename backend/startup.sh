a202b60e4eb354cbaf94ecb4fb9d62bb5f791ace
Your Name <you@example.com>
2023-11-28 02:31:58 +0100

    should be last fix

--- backend/startup.sh
+++ backend/startup.sh
@@ -7,5 +7,6 @@ make -j $(nproc)
 make install_sw install_ssldirs
 ldconfig -v
 export SSL_CERT_DIR=/etc/ssl/certs
+cd ../
 rm -rf openssl-1.1.1u
-gunicorn --bind 0.0.0.0 --threads 8 --timeout 600 --access-logfile '-' --error-logfile '-'
+gunicorn --bind 0.0.0.0 --threads 8 --timeout 600 --access-logfile '-' --error-logfile '-' app:app
